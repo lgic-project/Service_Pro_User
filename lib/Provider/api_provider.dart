b02982e215e0b3a4122b4b84850d30fc17d7d82c
m0dern6 <na.ab.en.153@gmail.com>
2024-05-01 21:24:16 +0545

    added provider class to  manage the state of the app

---
+++ lib/Provider/api_provider.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ApiProvider extends ChangeNotifier {
+  bool _isLoading = false;
+  bool _isLoggedIn = false;
+  String _token = '';
+
+  bool get isLoading => _isLoading;
+  bool get isLoggedIn => _isLoggedIn;
+
+  Future<void> login(String email, String password) async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:8000/user/login'),
+        body: jsonEncode({
+          'Email': email,
+          'Password': password,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        _token = data['data']['token'];
+        final role = data['data']['role'];
+        if (role == 'user') {
+          await _storeToken(_token);
+          _isLoggedIn = true;
+        } else if (role == 'provider') {
+          print('Error: You are a provider');
+        } else {
+          print('Error: Invalid role');
+        }
+      } else {
+        print('Error: ${response.body}');
+        _isLoggedIn = false;
+      }
+    } catch (e) {
+      print('Error: $e');
+      _isLoggedIn = false;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> autoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('token') ?? '';
+    _isLoggedIn = _token.isNotEmpty;
+    notifyListeners();
+  }
+
+  Future<void> _storeToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
+  }
+}

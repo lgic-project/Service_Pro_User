43c128f943adc95dd3fcc22cede294122fab278e
m0dern6 <na.ab.en.153@gmail.com>
2024-05-03 22:32:42 +0545

    added functional bottom navigation bar

--- lib/UI/home_screen/home_screen.dart
+++ lib/UI/home_screen/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:service_pro_user/UI/booking/booking.dart';
+import 'package:service_pro_user/UI/chat_screen/chat_screen.dart';
+import 'package:service_pro_user/UI/home_screen/default_home.dart';
+import 'package:service_pro_user/UI/profile/profile_page.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +12,82 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+  Color? unSelectedItemColor = Colors.pink[900];
+  Color selectedItemColor = Colors.white;
+  late Widget currentBody;
+
   @override
   Widget build(BuildContext context) {
+    switch (currentIndex) {
+      case 0:
+        currentBody = DefaultHome();
+        break;
+      case 1:
+        currentBody = Chat();
+        break;
+      case 2:
+        currentBody = Booking();
+        break;
+      case 3:
+        currentBody = ProfilePage();
+        break;
+    }
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Service Pro'),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        body: currentBody,
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Theme.of(context).primaryColor,
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/icons/home.png',
+                height: 30,
+                color:
+                    currentIndex == 0 ? selectedItemColor : unSelectedItemColor,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/icons/chat.png',
+                  height: 30,
+                  color: currentIndex == 1
+                      ? selectedItemColor
+                      : unSelectedItemColor),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/icons/booking.png',
+                  height: 30,
+                  color: currentIndex == 2
+                      ? selectedItemColor
+                      : unSelectedItemColor),
+              label: 'Booking',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('assets/icons/profile.png',
+                  height: 30,
+                  color: currentIndex == 3
+                      ? selectedItemColor
+                      : unSelectedItemColor),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: currentIndex,
+          selectedItemColor: selectedItemColor,
+          unselectedItemColor: unSelectedItemColor,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _MyAppState extends State<MyApp> {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primaryColor: Colors.teal,
         ),
         initialRoute: '/',
         routes: {

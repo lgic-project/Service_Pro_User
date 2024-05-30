79b14cbaa8460ef68ae717c10f86189531da6a8b
m0dern6 <na.ab.en.153@gmail.com>
2024-05-30 15:10:35 +0545

    signup method implemented

--- lib/UI/login_signup/signup_screen.dart
+++ lib/UI/login_signup/signup_screen.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:provider/provider.dart';
+import 'package:service_pro_user/Provider/signup_provider.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController addressController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  SignUpPage({super.key});
+  File? _image;
+  final picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Widget _buildImageContainer() {
+    return _image != null
+        ? Container(
+      width: 150,
+      height: 150,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.grey[300],
+      ),
+      child: CircleAvatar(
+        radius: 75,
+        backgroundImage: FileImage(_image!),
+      ),
+    )
+        : Container();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +55,17 @@ class SignUpPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('SERVICE PRO'),
+        backgroundColor: Color(0xFF43cbac),
       ),
       body: Container(
         height: screenHeight - appBarHeight,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/loginimage.jpg"),
-            fit: BoxFit.cover,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF43cbac), Color(0xFF006d77)],
           ),
         ),
         child: SingleChildScrollView(
@@ -32,75 +74,107 @@ class SignUpPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const SizedBox(height: 20),
-                const Text(
+                SizedBox(height: 40),
+                Text(
                   'Sign Up',
                   style: TextStyle(
-                    fontSize: 32,
+                    fontSize: 36,
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    fillColor: Colors.white,
-                    filled: true,
+                SizedBox(height: 20),
+                buildInputField(nameController, 'Name', Icons.person),
+                SizedBox(height: 10),
+                buildInputField(addressController, 'Address', Icons.location_on),
+                SizedBox(height: 10),
+                buildInputField(phoneNumberController, 'Phone Number', Icons.phone),
+                SizedBox(height: 10),
+                buildInputField(emailController, 'Email Address', Icons.email),
+                SizedBox(height: 10),
+                buildInputField(passwordController, 'Password', Icons.lock),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _pickImage,
+                  child: Text('Select Profile', style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF43cbac),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: addressController,
-                  decoration: const InputDecoration(
-                    labelText: 'Address',
-                    fillColor: Colors.white,
-                    filled: true,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: phoneNumberController,
-                  decoration: const InputDecoration(
-                    labelText: 'Phone Number',
-                    fillColor: Colors.white,
-                    filled: true,
-                  ),
-                  keyboardType: TextInputType.phone,
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email Address',
-                    fillColor: Colors.white,
-                    filled: true,
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    fillColor: Colors.white,
-                    filled: true,
-                  ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 20),
+                SizedBox(height: 20),
+                _buildImageContainer(),
+                SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
-                    // Handle sign up logic here
+                    final name = nameController.text;
+                    final address = addressController.text;
+                    final phoneNumber = phoneNumberController.text;
+                    final email = emailController.text;
+                    final password = passwordController.text;
+                    if (_image != null &&
+                        name.isNotEmpty &&
+                        address.isNotEmpty &&
+                        phoneNumber.isNotEmpty &&
+                        email.isNotEmpty &&
+                        password.isNotEmpty) {
+                      final imagePath = _image!.path;
+                      context.read<SignUpProvider>().signUp(
+                        name,
+                        email,
+                        password,
+                        phoneNumber,
+                        address,
+                        imagePath,
+                      );
+
+                      // Clear all text fields
+                      nameController.clear();
+                      addressController.clear();
+                      phoneNumberController.clear();
+                      emailController.clear();
+                      passwordController.clear();
+
+                      // Show pop-up message
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Success'),
+                            content: Text('Sign up successful!'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      print('Please fill all the fields');
+                    }
                   },
-                  child: const Text('Sign Up'),
+                  child: Text('Sign Up', style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF43cbac),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                 ),
-                const SizedBox(height: 10),
+                SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Text(
+                    Text(
                       'Already have an account?',
                       style: TextStyle(color: Colors.white),
                     ),
@@ -108,13 +182,35 @@ class SignUpPage extends StatelessWidget {
                       onPressed: () {
                         Navigator.pushReplacementNamed(context, '/login');
                       },
-                      child: const Text('Login'),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Color(0xFF43cbac),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildInputField(TextEditingController controller, String labelText, IconData icon) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        prefixIcon: Icon(icon, color: Color(0xFF43cbac)),
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
         ),
       ),
     );

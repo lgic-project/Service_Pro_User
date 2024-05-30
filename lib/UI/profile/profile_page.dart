e54f71d89c9191a0d2fc04a2c29e0947d08e9b7a
m0dern6 <na.ab.en.153@gmail.com>
2024-05-30 15:12:09 +0545

    added additional user data

--- lib/UI/profile/profile_page.dart
+++ lib/UI/profile/profile_page.dart
@@ -18,49 +18,107 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    final userProvider = Provider.of<LoginLogoutProvider>(context);
+    final Color primaryColor = Color(0xFF43cbac);
 
     return SafeArea(
       child: SingleChildScrollView(
           child: Consumer<ProfileProvider>(builder: (context, profile, child) {
+        Color activeColor = Colors.grey;
+        final activeStatus = profile.data['Active'];
+        if (activeStatus == true) {
+          activeColor = Colors.green;
+        } else if (activeStatus == false) {
+          activeColor = Colors.red;
+        }
         var user = profile.data;
         final profilePic = (user['Image'] ??
             'https://dudewipes.com/cdn/shop/articles/gigachad.jpg?v=1667928905&width=2048');
+
         return Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.4,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Colors.white, Colors.white],
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        primaryColor.withOpacity(0.8),
+                        activeColor,
+                      ],
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage('assets/background.png'),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.2), BlendMode.darken),
+                    ),
+                  ),
                 ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.25,
+                  left: MediaQuery.of(context).size.width / 2 - 50,
+                  child: CircleAvatar(
                     radius: 50,
                     backgroundImage: NetworkImage(profilePic.toString()),
                   ),
+                ),
+              ],
+            ),
+            SizedBox(height: 70),
+            Text(
+              user['Name'] ?? 'User Name',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            Text(
+              user['Email'] ?? 'User Email',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    child: ListTile(
+                      leading: Icon(Icons.person, color: primaryColor),
+                      title: Text('Account Information'),
+                      trailing:
+                          Icon(Icons.arrow_forward_ios, color: primaryColor),
+                      onTap: () {
+                        // Navigate to account information page
+                      },
+                    ),
+                  ),
                   SizedBox(height: 10),
-                  Text(
-                    user['Name'] ?? 'User Name',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+                  Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    child: ListTile(
+                      leading: Icon(Icons.settings, color: primaryColor),
+                      title: Text('Settings'),
+                      trailing:
+                          Icon(Icons.arrow_forward_ios, color: primaryColor),
+                      onTap: () {
+                        // Navigate to settings page
+                      },
                     ),
                   ),
-                  Text(
-                    user['Email'] ?? 'User Email',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
-                    ),
-                  ),
+                  SizedBox(height: 10),
                 ],
               ),
             ),

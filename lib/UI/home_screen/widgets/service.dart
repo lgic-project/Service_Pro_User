086f33255b9971f84dbec82825ba945b8f0d7002
m0dern6 <na.ab.en.153@gmail.com>
2024-06-02 14:03:48 +0545

    service providers shown under each service

--- lib/UI/home_screen/widgets/service.dart
+++ lib/UI/home_screen/widgets/service.dart
@@ -2,10 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
-import 'package:provider/provider.dart';
-import 'package:provider/single_child_widget.dart';
 import 'package:service_pro_user/Models/category_model.dart';
-import 'package:service_pro_user/Provider/chat_user_provider.dart';
+import 'package:service_pro_user/UI/service_providers/service_providers.dart';
 
 class Service extends StatefulWidget {
   final CategoryModel category;
@@ -80,11 +78,12 @@ class _ServiceState extends State<Service> {
                         ],
                       ),
                       child: GestureDetector(
-                        onTap: (){
+                        onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => ServiceProviders(category: widget.category),
+                              builder: (context) =>
+                                  ServiceProviders(serviceData: serviceData),
                             ),
                           );
                         },
@@ -162,81 +161,3 @@ class _ServiceState extends State<Service> {
     );
   }
 }
-
-class ServiceProviders extends StatefulWidget {
-  final CategoryModel category;
-  const ServiceProviders({Key? key, required this.category});
-
-  @override
-  _ServiceProvidersState createState() => _ServiceProvidersState();
-}
-
-class _ServiceProvidersState extends State<ServiceProviders> {
-  List<dynamic> filteredServiceProviders = [];
-
-  @override
-  void initState() {
-    super.initState();
-    filterServiceProviders();
-  }
-
-  // Method to filter service providers based on service IDs
-  void filterServiceProviders() {
-    // Get the list of service providers from the provider
-    List<dynamic> allServiceProviders =
-        Provider.of<ChatUserProvider>(context, listen: false).users;
-
-    // Get the service IDs from the current category
-    List<String> serviceIds = widget.category.services!
-        .map((service) => service['serviceId'].toString())
-        .toList();
-
-    // Filter service providers based on service IDs
-    filteredServiceProviders = allServiceProviders
-        .where((provider) =>
-    provider['Services'] != null &&
-        provider['Services'].any((serviceId) => serviceIds.contains(serviceId)))
-        .toList();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Service Providers'),
-      ),
-      body: ListView.builder(
-        itemCount: filteredServiceProviders.length,
-        itemBuilder: (context, index) {
-          // Access the details of each filtered service provider
-          var serviceProvider = filteredServiceProviders[index];
-          String name = serviceProvider['Name'] ?? 'Name not available';
-          String email = serviceProvider['Email'] ?? 'Email not available';
-          String phoneNumber = serviceProvider['PhoneNo']?.toString() ?? 'Phone number not available';
-          String address = serviceProvider['Address'] ?? 'Address not available';
-          String imageUrl = serviceProvider['Image'] != null && serviceProvider['Image'].isNotEmpty
-              ? serviceProvider['Image'][0]
-              : 'https://via.placeholder.com/150'; // Placeholder image URL
-
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(imageUrl),
-            ),
-            title: Text(name),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(email),
-                Text(phoneNumber),
-                Text(address),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-
-
---
+++ lib/UI/service_providers/service_providers.dart
@@ -0,0 +1,189 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ServiceProviders extends StatefulWidget {
+  final dynamic serviceData;
+  const ServiceProviders({Key? key, required this.serviceData})
+      : super(key: key);
+
+  @override
+  State<ServiceProviders> createState() => _ServiceProvidersState();
+}
+
+class _ServiceProvidersState extends State<ServiceProviders> {
+  List<dynamic> serviceProviders = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchServiceData();
+  }
+
+  Future<void> fetchServiceData() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      final response = await http.get(Uri.parse(
+          'http://20.52.185.247:8000/user/service/${widget.serviceData['_id']}'));
+      if (response.statusCode == 200) {
+        setState(() {
+          serviceProviders = jsonDecode(response.body)['data'] as List;
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load service data');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      print('Error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Service Providers'),
+        backgroundColor: Color(0xFF43CBAC),
+      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.teal),
+              ),
+            )
+          : serviceProviders.isEmpty
+              ? Center(
+                  child: Text(
+                    'No service providers found',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal,
+                    ),
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: serviceProviders.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Card(
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        color: Colors.white,
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(20),
+                          onTap: () {
+                            // Add onTap functionality
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.all(16),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                    serviceProviders[index]['Image'].toString(),
+                                  ),
+                                  radius: 40,
+                                ),
+                                SizedBox(width: 20),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        serviceProviders[index]['Name']
+                                            .toString(),
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.teal,
+                                        ),
+                                      ),
+                                      SizedBox(height: 8),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.location_on,
+                                            color: Colors.teal,
+                                            size: 20,
+                                          ),
+                                          SizedBox(width: 8),
+                                          Expanded(
+                                            child: Text(
+                                              serviceProviders[index]['Address']
+                                                  .toString(),
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                color: Colors.grey[700],
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(height: 8),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.phone,
+                                            color: Colors.teal,
+                                            size: 20,
+                                          ),
+                                          SizedBox(width: 8),
+                                          Text(
+                                            serviceProviders[index]['PhoneNo']
+                                                .toString(),
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.grey[700],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(height: 8),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.email,
+                                            color: Colors.teal,
+                                            size: 20,
+                                          ),
+                                          SizedBox(width: 8),
+                                          Expanded(
+                                            child: Text(
+                                              serviceProviders[index]['Email']
+                                                  .toString(),
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                color: Colors.grey[700],
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+    );
+  }
+}

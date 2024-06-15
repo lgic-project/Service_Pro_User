78cd5de8ee9d176f0548d0c345c067196809221a
Sajeet07 <sajitgurung829@gmail.com>
2024-06-15 20:03:44 +0545

    worked on notification screem

--- lib/UI/login_signup/login_screen.dart
+++ lib/UI/login_signup/login_screen.dart
@@ -15,206 +15,254 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-
   bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     final userProvider = Provider.of<LoginLogoutProvider>(context);
-    final double screenHeight = MediaQuery.of(context).size.height;
-    final double appBarHeight = AppBar().preferredSize.height;
-
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('SERVICE PRO'),
-        backgroundColor: Color(0xFF43cbac),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/dashboard');
-            },
-            icon: const Icon(Icons.close),
-          )
-        ],
-      ),
-      body: Container(
-        height: screenHeight - appBarHeight,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF43cbac), Color(0xFF006d77)],
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.greenAccent, Color.fromARGB(255, 90, 251, 96)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
           ),
-        ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 40),
-                Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+          // Bubble shapes
+          Positioned(
+            top: 50,
+            left: 30,
+            child: Bubble(color: Colors.blue, size: 60),
+          ),
+          Positioned(
+            top: 150,
+            right: 30,
+            child: Bubble(color: Colors.purple, size: 80),
+          ),
+          Positioned(
+            bottom: 100,
+            left: 50,
+            child: Bubble(color: Color.fromARGB(255, 240, 105, 226), size: 100),
+          ),
+          Positioned(
+            bottom: 200,
+            right: 50,
+            child: Bubble(color: Colors.pinkAccent, size: 60),
+          ),
+          // Login Form
+          Center(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              margin: EdgeInsets.symmetric(horizontal: 32),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 20,
+                    offset: Offset(0, 10),
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 20),
-                buildInputField(_emailController, 'Email Address', Icons.email),
-                SizedBox(height: 10),
-                buildInputField(
-                  _passwordController,
-                  'Password',
-                  Icons.lock,
-                  isPasswordField: true,
-                  isPasswordVisible: _isPasswordVisible,
-                  onVisibilityToggle: () {
-                    setState(() {
-                      _isPasswordVisible = !_isPasswordVisible;
-                    });
-                  },
-                ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(
-                            context, '/forgotPassword');
-                      },
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Username or email',
+                      labelStyle: TextStyle(color: Colors.white),
+                      prefixIcon: Icon(Icons.person, color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.2),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: !_isPasswordVisible,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.white),
+                      prefixIcon: Icon(Icons.lock, color: Colors.white),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
+                      ),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.2),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                            checkColor: Colors.blue,
+                            fillColor: MaterialStateProperty.all(Colors.white),
+                          ),
+                          Text('Remember me',
+                              style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, '/forgotPassword');
+                        },
+                        child: Text(
+                          'Forgot password?',
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 241, 241, 239)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await userProvider.login(
+                        _emailController.text,
+                        _passwordController.text,
+                      );
+
+                      // Check if the login was successful
+                      if (userProvider.isLoggedIn) {
+                        if (userProvider.verified) {
+                          Navigator.pushReplacementNamed(context, '/dashboard');
+                        } else {
+                          await Provider.of<SignUpProvider>(context,
+                                  listen: false)
+                              .sendVerificationEmail(
+                                  context, _emailController.text)
+                              .then((_) => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Verification(
+                                            email: _emailController.text)),
+                                  ));
+                        }
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Error'),
+                              content: const Text('Invalid email or password'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.orange,
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(
                       child: Text(
-                        'Forgot Password?',
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpPage(),
+                        ),
+                      );
+                    },
+                    child: Text.rich(
+                      TextSpan(
+                        text: "Don't have an account? ",
+                        children: [
+                          TextSpan(
+                            text: 'Sign up',
+                            style: TextStyle(color: Colors.orange),
+                          ),
+                        ],
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 5),
-                ElevatedButton(
-                  onPressed: () async {
-                    await userProvider.login(
-                      _emailController.text,
-                      _passwordController.text,
-                    );
-
-                    // Check if the login was successful
-                    if (userProvider.isLoggedIn) {
-                      if (userProvider.verified) {
-                        Navigator.pushReplacementNamed(context, '/dashboard');
-                      } else {
-                        await Provider.of<SignUpProvider>(context,
-                                listen: false)
-                            .sendVerificationEmail(
-                                context, _emailController.text)
-                            .then((_) => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Verification(
-                                          email: _emailController.text)),
-                                ));
-                      }
-                    } else {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text('Error'),
-                            content: const Text('Invalid email or password'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    }
-                  },
-                  child: userProvider.isLoading
-                      ? const CircularProgressIndicator()
-                      : Text(
-                          'LOGIN',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF43cbac),
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SignUpPage(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          color: Color(0xFF43cbac),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
+}
 
-  Widget buildInputField(
-    TextEditingController controller,
-    String labelText,
-    IconData icon, {
-    bool isPasswordField = false,
-    bool isPasswordVisible = false,
-    Function()? onVisibilityToggle,
-  }) {
-    return TextFormField(
-      controller: controller,
-      obscureText: isPasswordField && !isPasswordVisible,
-      decoration: InputDecoration(
-        labelText: labelText,
-        prefixIcon: Icon(icon, color: Color(0xFF43cbac)),
-        filled: true,
-        fillColor: Colors.white,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide.none,
-        ),
-        suffixIcon: isPasswordField
-            ? IconButton(
-                onPressed: onVisibilityToggle,
-                icon: isPasswordVisible
-                    ? Icon(Icons.visibility, color: Color(0xFF43cbac))
-                    : Icon(Icons.visibility_off, color: Color(0xFF43cbac)),
-              )
-            : null,
+class Bubble extends StatelessWidget {
+  final Color color;
+  final double size;
+
+  Bubble({required this.color, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.5),
+        shape: BoxShape.circle,
       ),
     );
   }
